56158c8c5a3d7d986d199abd073ead86b37987c2
orkhaner <orkhanrrahimli@gmail.com>
2025-02-13 08:36:03 +0100

    Tutorial Finished.

--- CPPPPAdvancedProgramming9/Program.cs
+++ CPPPPAdvancedProgramming9/Program.cs
@@ -20,8 +20,8 @@
             /*Like*/
             IEnumerable<object> obj2 = new List<String>();
             //Contravariance is the opposite of Covariance.
-            //For Contravariance's Example, I will finish the deligates tutorial and will be back here as this definition is known with deligates.
-           
+            //Check the Delegates part for the contravariance illustration.
+            
             
             //Covariance in Arrays
             //It is risky for type safety
@@ -40,6 +40,22 @@
             //We basically need to add out keyword to the generic parameters to utilize from Covariance.
             //This feature is merely applicable to interfaces and delegates as far as I have learned until now.
             IAnimal<object> str = new Dog<String>{ };
+            
+            
+            //Covariance in Delegates
+            //It requires out keyword in the generic parameter in the delegate declaration as the generics do.
+            Sinif1 Sinif1Obj = new Sinif1();
+            Sinif2 Sinif2Obj = new Sinif2();
+            ExpHandler<Sinif1> ExpDelegate1 = Sinif1Obj.ExpMethod<Sinif1>;
+            ExpHandler<Sinif2> ExpDelegate2 = Sinif2Obj.ExpMethod<Sinif2>;
+            ExpDelegate1 = ExpDelegate2;
+            
+            //Contravariance in Delegates (Generics)
+            ExpHandler1<Sinif1> ExpDelegate11 = Sinif1Obj.ExpMethod<Sinif1>;
+            ExpHandler1<Sinif2> ExpDelegate21 = Sinif2Obj.ExpMethod<Sinif2>;
+            ExpDelegate21 = ExpDelegate11; 
+            //Although Sinif2 is the derived class, the delegate with the derived class argument
+            //can reference another one's instance with the base class generic argument.
         }
     }
     class Animal{public virtual object getAnimal() { return null; }}
@@ -51,4 +67,11 @@
     interface IAnimal<out t>{} //Remove out keyword to see the error at 41st line
     class Dog<t> : IAnimal<t> {}
     
+    
+    //Delegates 
+    class Sinif1 { public virtual void ExpMethod<t1>() { } }
+
+    class Sinif2 : Sinif1 { public override void ExpMethod<t1>() {} }
+    public delegate void ExpHandler<out t1>();
+    public delegate void ExpHandler1<in t1>(); //We use in keyword for the contravariance behaviour
 }
